df5aa67d91561847b280ff3b99616f96b55d5f0b
Arun Jamal <arun.jamal31@gmail.com>
2020-12-31 21:06:13 -0700

    Show Temperature on the Page and Add Weather Logo

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import weatherLogo from "./weatherLogo.png";
 import "./App.css";
 
 function App() {
   var city = "Edmonton";
-  var countryCode = "CA";
+  var countryCode = "Canada";
   const apiKey = "cecfe61da45b46b0ff77615eed1fd730";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&units=metric&appid=${apiKey}`;
+  const [weather, setWeather] = useState("");
 
   fetch(url)
     .then((response) => response.json())
     .then(function (data) {
-      var temp = Math.round(data["main"].temp) + "°C";
-      console.log("The weather in Edmonton right now is " + temp + ".");
+      var temperature = Math.round(data["main"].temp);
+      setWeather(temperature);
     });
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={weatherLogo} alt=""/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The weater right now in {city}, {countryCode} is <b>{weather}°C</b>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
