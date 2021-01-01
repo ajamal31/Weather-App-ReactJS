98e587a0e8e71f9674c9e568662ecb55a81560b7
Arun Jamal <arun.jamal31@gmail.com>
2020-12-31 20:28:26 -0700

    Format App.Js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  var city = 'Edmonton';
-  var countryCode = 'CA';
-  const apiKey = 'cecfe61da45b46b0ff77615eed1fd730';
+  var city = "Edmonton";
+  var countryCode = "CA";
+  const apiKey = "cecfe61da45b46b0ff77615eed1fd730";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&units=metric&appid=${apiKey}`;
-  
-  fetch(url).then((response) => response.json())
+
+  fetch(url)
+    .then((response) => response.json())
     .then(function (data) {
-      var temp = Math.round(data['main'].temp) + '°C';
-      console.log('The weather in Edmonton right now is ' + temp + '.');
+      var temp = Math.round(data["main"].temp) + "°C";
+      console.log("The weather in Edmonton right now is " + temp + ".");
     });
 
   return (
