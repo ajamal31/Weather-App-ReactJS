f08f9a844055aae74402b34b69d85f5117edb640
Arun Jamal <arun.jamal31@gmail.com>
2021-01-01 17:33:21 -0700

    Move fetch method into a function, Add search bar

--- src/App.js
+++ src/App.js
@@ -3,47 +3,72 @@ import weatherLogo from "./weatherLogo.png";
 import "./App.css";
 
 function App() {
-  var city = "Edmonton";
-  var countryCode = "Canada";
-  const apiKey = "cecfe61da45b46b0ff77615eed1fd730";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&units=metric&appid=${apiKey}`;
-  const [weather, setWeather] = useState("");
-  const [icon, setIcon] = useState(null);
-  const [time, setTime] = useState("");
+	const [city, setCity] = useState("Edmonton");
+	const [countryCode, setCountryCode] = useState("Canada");
+	const apiKey = "cecfe61da45b46b0ff77615eed1fd730";
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&units=metric&appid=${apiKey}`;
+	const [weather, setWeather] = useState("");
+	const [icon, setIcon] = useState(null);
+	const [time, setTime] = useState("");
+	const [keyword, setKeyword] = useState("Search ...");
+	const defaultMsg = "Search ...";
+	const defaultCity = "Edmonton";
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      var temperature = Math.round(data["main"].temp);
-      var iconUrl = `http://openweathermap.org/img/wn/${data["weather"][0].icon}@2x.png`;
-      var dateTime = new Date(data["dt"] * 1000).toLocaleString();
-      setWeather(temperature);
-      setIcon(iconUrl);
-      setTime(dateTime);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+	function init() {
+		fetch(url)
+			.then((response) => response.json())
+			.then((data) => {
+				var temperature = Math.round(data["main"].temp);
+				var iconUrl = `http://openweathermap.org/img/wn/${data["weather"][0].icon}@2x.png`;
+				var dateTime = new Date(data["dt"] * 1000).toLocaleString();
+				setWeather(temperature);
+				setIcon(iconUrl);
+				setTime(dateTime);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <h3 style={{ color: "lightgreen" }}>
-            Arun's Weather App {""}
-            <img className="App-logo" src={weatherLogo} alt="" />
-          </h3>
-        </div>
-        <img src={icon} alt="" />
-        <b style={{ color: "orange" }}>{weather}°C</b>
+	// init();
 
-        <p style={{ color: "lightpink" }}>
-          {city}, {countryCode}
-        </p>
-        <b style={{ color: "lightblue" }}>{time}</b>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				{" "}
+				<h3 style={{ color: "lightgreen" }}>
+					Arun's Weather App {""}
+					<img className="App-logo" src={weatherLogo} alt="" />
+				</h3>
+			</header>
+			<div className="App-body">
+				<p style={{ color: "lightyellow" }}>{keyword}</p>
+				<input
+					className="App-input"
+					type="text"
+					placeholder={keyword}
+					onChange={(e) => {
+						var input = e.target.value;
+						if (input) {
+							setKeyword(input);
+							// Use the following after implementing location autoimplement:
+							// setCity(input);
+						} else {
+							setKeyword(defaultMsg);
+							// Use the following after implementing location autoimplement:
+							// setCity(defaultCity);
+						}
+					}}
+				/>
+				<img src={icon} alt="" />
+				<b style={{ color: "orange" }}>{weather}°C</b>
+				<p style={{ color: "lightpink" }}>
+					{city}, {countryCode}
+				</p>
+				<b style={{ color: "lightblue" }}>{time}</b>
+			</div>
+		</div>
+	);
 }
 
 export default App;
